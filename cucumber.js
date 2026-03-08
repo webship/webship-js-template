22b8e61c0070977f105c485694715b61c1f183ff
Tasneem Natshah <t.natshah@gmail.com>
2026-03-08 16:23:57 +0300

    Update cucumber.js file and add tsconfig.json file #5

--- cucumber.js
+++ cucumber.js
@@ -12,7 +12,6 @@ module.exports = {
       'json:tests/reports/cucumber_report.json',
     ],
     formatOptions: {
-      colorsEnabled: true,
       theme: {
         'feature keyword': ['bold', 'blue'],
         'feature name': ['blue', 'underline'],
